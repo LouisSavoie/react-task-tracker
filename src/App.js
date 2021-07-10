73ff00cf339bb799a4b840d2906a195003d6c8f5
Louis Savoie <thelousavoie@gmail.com>
2021-07-09 20:20:25 -0400

    add reminder toggling

--- src/App.js
+++ src/App.js
@@ -30,10 +30,19 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks Here'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+          'No Tasks Here'
+      )}
     </div>
   );
 
